37b9b59a9349d47404a6aaf5c3a7fb326526e167
entyo <entyo.contact@gmail.com>
2016-05-29 22:23:20 +0900

    ふぁすこみっ！

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(f,s){s=document.createElement("script");s.src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js";s.onload=function(){f(jQuery.noConflict(false))};document.body.appendChild(s)})(function($){jQuery(document).ready(function(){$(".emoji-only").css({"font-size":"5rem"});})});
